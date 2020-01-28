2b7cfa697266d42b5ea7f14ba53529cf059ef0e8
ananyaanu <31927505+ananyaanu@users.noreply.github.com>
2020-01-28 13:04:31 +0530

    Update App.java

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! poll SCM" );
+        System.out.println( "Hello World! web hook" );
     }
 }
